5ec853784309aa90215ede8c288798fa32332473
combark-max <combark@gmail.com>
2026-04-29 17:25:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', e => {
-  console.log('SW installed');
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("app").then(cache=>{
+      return cache.addAll(["./","index.html"]);
+    })
+  );
 });
